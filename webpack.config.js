38a6d92c1ba645f5540e49138d192aee45865ca1
huitoutunao <lh_huitoutunao@163.com>
2021-08-12 12:00:46 +0800

    feat: 编写说明文档和代码注释

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path')
 
 module.exports = {
+    // 开发模式
     mode: 'development',
+
+    // 入口配置
     entry: {
         app: './src/main.js'
     },
+
+    // 输出配置
     output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        // 输出文件名称
+        // [name] 指入口名称
+        // [contenthash] 指 hash 值，默认 20 位
+        filename: '[name].[contenthash:16].js',
+        path: path.resolve(__dirname, 'dist')
     }
 }
